05326e5e1798e0eae8279c55386b980a672ba206
Godfrey-Alimony <alimonyg@gmail.com>
2021-08-21 12:40:07 +0300

    js,html updated

--- app.js
+++ app.js
@@ -11,21 +11,46 @@ const getComputerPlay = () => {
 
 const gameLogic = (user) => {
     const computer = getComputerPlay();
-    //logic
-    if (computer == "rock" && user == "rock")
-    result = resultArray[0];
-    console.log("result: draw");
-
-    
-    console.log("computer choice:",computer);
+     //logic
+    if (computer == "rock" && user == "rock"){
+        result = resultArray[0];
+        console.log("result: draw");
+    }else if (computer == "scissors" && user == "rock"){
+        result = resultArray[1];
+        console.log("result: win");
+    }else  if(computer == "paper" && user == "rock") {
+        result = resultArray[2];
+        console.log("result: lose");
+    }else if (computer == "rock" && user == "paper"){
+        result = resultArray[1];
+        console.log("result: win");
+    }else if (computer == "scissors" && user == "paper"){
+        result = resultArray[2];
+        console.log("result:lose");
+    }else if (computer == "paper" && user == "paper"){
+        result = resultArray[0];
+        console.log("result: draw");
+    }else if (computer =="rock" && user == "scissors"){
+        result = resultArray[2];
+        console.log("result: lose");
+    }else if (computer == "scissors" && user == "scissors"){
+        result = resultArray[0];
+        console.log("result:draw");
+    }else if (computer == "paper" && user == "scissors"){
+        result = resultArray[1];
+        console.log("result: win");
+    }
+     
+    console.log("computer Play:",computer);
     document.getElementById("computersChoice").innerHTML = computer;
+    document.getElementById("result").innerHTML = result;
 };
 
 const rockOption = document
 .getElementById("rock")
 .addEventListener("click",function() {
     console.log("rock was clicked!");
-    document.getElementById("userChoice").innerHTML ="rock"
+    document.getElementById("userChoice").innerHTML ="rock";
     gameLogic("rock");
 });
 
@@ -33,12 +58,23 @@ const paperOption = document
 .getElementById("paper")
 .addEventListener("click",function() {
     console.log("paper was clicked!");
-    document.getElementById("userChoice").innerHTML ="paper"
+    document.getElementById("userChoice").innerHTML ="paper";
+    gameLogic("paper");
 });
 
 const scissorsOption = document
 .getElementById("scissors")
 .addEventListener("click",function() {
     console.log("scissors was clicked!");
-    document.getElementById("userChoice").innerHTML ="scissors"
+    document.getElementById("userChoice").innerHTML ="scissors";
+    gameLogic("scissors");
+});
+
+const reset = document
+.getElementById("reset")
+.addEventListener("click", function() {
+    console.log("Reset");
+    document.getElementById("computersChoice").innerHTML = "";
+    document.getElementById("userChoice").innerHTML ="";
+    document.getElementById("result").innerHTML ="";
 });
